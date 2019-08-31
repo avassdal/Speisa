a0e90e7dcf32a857624fa9d7ec071bd61e14fabf
Aleksander Vassdal <aleksander@lydfirmaetekko.no>
2019-08-31 23:18:02 +0200

    fix error

--- PI/lyd1.py
+++ PI/lyd1.py
@@ -39,7 +39,7 @@ async def run(loop):
             c = b.decode('utf8', errors='ignore')[:-2]
             d = c.replace('.', '')
             print (d)
-            if int(c) < int(100):
+            if int(d) < int(100):
                 print ("Yes!")
                 music.play()
                 
