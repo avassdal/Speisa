ff6b5f7d7225600652812e4b02de2ecd4bbd7976
Speisa <speisa@pop-os.localdomain>
2019-09-14 20:13:26 +0200

    Lyd8

---
+++ PI/lyd8.py
@@ -0,0 +1,52 @@
+import asyncio
+from datetime import time
+from idlelib import rstrip
+from signal import pause
+from time import sleep
+import pygame
+from bleak import BleakClient, discover
+
+bleaddress='CB:C1:7E:4B:03:72'
+serviceUuid = '6e400001-b5a3-f393-e0a9-e50e24dcca9e'
+characteristicUuid = '6e400003-b5a3-f393-e0a9-e50e24dcca9e'
+
+pygame.mixer.pre_init(44100, -16, 2, 512)
+pygame.mixer.init()
+pygame.init()
+music=pygame.mixer.Sound("lyd8.ogg")
+
+
+async def run(loop):
+    device = None
+
+    print('looking for devices...')
+    while device is None:
+        devices = await discover()
+        device = next((d for d in devices if d.name == 'Lyd8'), None)
+
+    async with BleakClient(device.address, loop=loop) as client:
+        queue = asyncio.LifoQueue(maxsize=1)
+        print('disconnecting...')
+        await client.disconnect()
+        print('connecting...')
+        await client.connect()
+        
+        print('starting notificiations')
+        def callback(sender, data):
+          print(f"{sender}: {data}")
+        await client.start_notify(characteristicUuid, lambda _, data: loop.call_soon(queue.put_nowait, data))
+
+        print('waiting')
+        while True:
+            args = await queue.get()
+            music.play()
+            print(args)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    
+    try:
+        loop.run_until_complete(run(loop))
+    except KeyboardInterrupt:
+            loop.close()
